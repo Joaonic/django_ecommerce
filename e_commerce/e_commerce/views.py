056cb9969bc7ccc13bea2c26440114e777ca9b48
root <root@localhost.localdomain>
2020-07-04 21:15:02 -0400

    change imports

--- e_commerce/e_commerce/views.py
+++ e_commerce/e_commerce/views.py
@@ -2,6 +2,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def home_page(request):
-    return render(request, "home_page.html", {})
-   ## html_ =  """"""
-   ## return HttpResponse(html_)
+    context = {
+        "title": "Página principal",
+        "content": "Bem-vindo a página principal"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title": "Página sobre",
+        "content": "Bem-vindo a página sobre"
+    }
+    return render(request, "about/view.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Página de contato",
+        "content": "Bem-vindo a página de contato"
+    }
+    return render(request, "contact/view.html", context)
