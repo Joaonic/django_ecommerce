f9521675264848ee1f4879e0983b7c3a22de1188
root <root@localhost.localdomain>
2020-07-13 18:18:30 -0400

    Class ProductListView created

--- e_commerce/e_commerce/urls.py
+++ e_commerce/e_commerce/urls.py
@@ -22,13 +22,16 @@ from django.urls import path
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
+from products.views import ProductListView
+
 urlpatterns = [
 	path('', home_page),
 	path('about/', about_page),
 	path('contact/', contact_page),
-        path('login/', login_page),
-        path('register/', register_page),
-	path('admin/', admin.site.urls),
+    path('login/', login_page),
+    path('register/', register_page),
+	path('products/', ProductListView.as_view()),
+    path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
